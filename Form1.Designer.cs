9596814c3c0d7d1c23610e82a95b976680c61c7d
Chris Home <Chris Home@DESKTOP-AN49DKJ>
2022-10-30 22:21:13 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace SamecDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblIdNumber = new System.Windows.Forms.Label();
+            this.txtIdNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(325, 147);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(136, 37);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.bisaya_Click);
+            // 
+            // lblIdNumber
+            // 
+            this.lblIdNumber.AutoSize = true;
+            this.lblIdNumber.Location = new System.Drawing.Point(218, 58);
+            this.lblIdNumber.Name = "lblIdNumber";
+            this.lblIdNumber.Size = new System.Drawing.Size(65, 15);
+            this.lblIdNumber.TabIndex = 1;
+            this.lblIdNumber.Text = "ID Number";
+            // 
+            // txtIdNumber
+            // 
+            this.txtIdNumber.Location = new System.Drawing.Point(310, 55);
+            this.txtIdNumber.Name = "txtIdNumber";
+            this.txtIdNumber.Size = new System.Drawing.Size(180, 23);
+            this.txtIdNumber.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtIdNumber);
+            this.Controls.Add(this.lblIdNumber);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblIdNumber;
+        private System.Windows.Forms.TextBox txtIdNumber;
+    }
+}
+
